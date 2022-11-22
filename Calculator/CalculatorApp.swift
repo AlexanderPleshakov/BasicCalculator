73ebf48b018ffc46180f38da3d32c4a2d11358d2
Pleshakov <sanya.pleshakov.95@mail.ru>
2022-11-22 20:58:54 +0300

    project

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Александр Плешаков on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
